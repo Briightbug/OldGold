2159184e605c06b69af609ab11e287d4f83f9790
Briightbug <145158148+Briightbug@users.noreply.github.com>
2026-09-12 02:00:58 +0200

    Added player sprite, play animations WIP

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -2,18 +2,26 @@ class_name Player
 extends CharacterBody2D
 
 @export var move_speed = 10.0
+@export var move_speed_multiplier = 100.0
 
-var direction = 0
+var direction : Vector2
 
-func _process(_delta: float) -> void:
-	print(str(direction))
-
-func _physics_process(_delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	direction = Input.get_axis("Left", "Right")
-	if direction:
-		velocity.x = direction * move_speed
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("Up"):
+		direction.y = -move_speed
+	elif Input.is_action_pressed("Down"):
+		direction.y = move_speed
+	else:
+		direction.y = 0
 	
-
+	if Input.is_action_pressed("Left"):
+		direction.x = -move_speed
+	elif Input.is_action_pressed("Right"):
+		direction.x = move_speed
+	else:
+		direction.x = 0
+	
+	direction = direction.normalized()
+	velocity = direction * move_speed * move_speed_multiplier * delta
+	
 	move_and_slide()
